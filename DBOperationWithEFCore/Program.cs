8e7dcbf4070f9b542dc60c6b077fb8141fe33577
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2024-12-05 00:04:09 +0530

    DbContext and Connection String Setup

--- DBOperationWithEFCore/Program.cs
+++ DBOperationWithEFCore/Program.cs
@@ -1,4 +1,10 @@
+using DBOperationWithEFCore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
