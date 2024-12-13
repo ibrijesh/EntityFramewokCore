ab37f63652297565b5257c26ba7eff935b968135
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2024-12-13 20:06:27 +0530

    Insert/Add new record

---
+++ DBOperationWithEFCore/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+using DBOperationWithEFCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBOperationWithEFCore.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BookController(AppDbContext _appDbContext) : ControllerBase
+    {
+        [HttpPost("")]
+        public async Task<IActionResult> AddBooksAsync([FromBody] Book model)
+        {
+            _appDbContext.Books.Add(model);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(model);
+        }
+    }
+}
