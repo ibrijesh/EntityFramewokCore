830bbd8274f24a2cac75f24eeac934b6f8ddd905
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2024-12-23 13:18:08 +0530

    Update record using Entity Framework Core in Single Database
    Drawback - It will reset the column value to default if value not passes in payload.

--- DBOperationWithEFCore/Controllers/BookController.cs
+++ DBOperationWithEFCore/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 using DBOperationWithEFCore.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DBOperationWithEFCore.Controllers
 {
@@ -20,6 +21,14 @@ namespace DBOperationWithEFCore.Controllers
         public async Task<IActionResult> AddBookInBulkAsync([FromBody] List<Book> model)
         {
             _appDbContext.Books.AddRange(model);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateBookAsync([FromBody] Book model)
+        {
+            _appDbContext.Books.Update(model);
             await _appDbContext.SaveChangesAsync();
             return Ok(model);
         }
